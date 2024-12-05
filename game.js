933f086c7c15e3191830aa22af9abb32a9bdb197
Gaurav Sheth <gauravsheth2001@gmail.com>
2024-12-05 03:14:18 -0500

    till part 3

---
+++ game.js
@@ -0,0 +1,309 @@
+var INPUT_OBJECT_URL = "objects.json";
+
+// get the JSON file from the passed URL
+function getJSONFile(url,descr) {
+    try {
+        if ((typeof(url) !== "string") || (typeof(descr) !== "string"))
+            throw "getJSONFile: parameter not a string";
+        else {
+            var httpReq = new XMLHttpRequest(); // a new http request
+            httpReq.open("GET",url,false); // init the request
+            httpReq.send(null); // send the request
+            var startTime = Date.now();
+            while ((httpReq.status !== 200) && (httpReq.readyState !== XMLHttpRequest.DONE)) {
+                if ((Date.now()-startTime) > 3000)
+                    break;
+            } // until its loaded or we time out after three seconds
+            if ((httpReq.status !== 200) || (httpReq.readyState !== XMLHttpRequest.DONE))
+                throw "Unable to open "+descr+" file!";
+            else
+                return JSON.parse(httpReq.response); 
+        } // end if good params
+    } // end try    
+    
+    catch(e) {
+        console.log(e);
+        return(String.null);
+    }
+} // end get input json file
+
+// set up the webGL environment
+function setupWebGL() {
+
+    var imageCanvas = document.getElementById("myImageCanvas"); // create a 2d canvas
+        var cw = imageCanvas.width, ch = imageCanvas.height; 
+        imageContext = imageCanvas.getContext("2d"); 
+        var bkgdImage = new Image(); 
+        bkgdImage.crossOrigin = "Anonymous";
+        bkgdImage.src = "galaxy.jpg";
+        bkgdImage.onload = function(){
+            var iw = bkgdImage.width, ih = bkgdImage.height;
+            imageContext.drawImage(bkgdImage,0,0,iw,ih,0,0,cw,ch);   
+    }
+
+     
+    // Get the canvas and context
+    var canvas = document.getElementById("myWebGLCanvas"); // create a js canvas
+    gl = canvas.getContext("webgl"); // get a webgl object from it
+    
+    try {
+      if (gl == null) {
+        throw "unable to create gl context -- is your browser gl ready?";
+      } else {
+        gl.clearColor(0.0, 0.0, 0.0, 1.0); // use black when we clear the frame buffer
+        gl.clearDepth(1.0); // use max when we clear the depth buffer
+        gl.enable(gl.DEPTH_TEST); // use hidden surface removal (with zbuffering)
+      }
+    } // end try
+    
+    catch(e) {
+      console.log(e);
+    } // end catch
+ 
+} // end setupWebGL
+
+// read models in, load them into webgl buffers
+function loadModels() {
+    inputObjects = getJSONFile(INPUT_OBJECT_URL,"objects");
+
+    try {
+        if (inputTriangles == String.null)
+            throw "Unable to load triangles file!";
+        else {
+            var whichSetVert; // index of vertex in current triangle set
+            var whichSetTri; // index of triangle in current triangle set
+            var vtxToAdd; // vtx coords to add to the coord array
+            var normToAdd; // vtx normal to add to the coord array
+            var textureToAdd; // vtx texture to add to the coord array
+
+            var triToAdd; // tri indices to add to the index array
+            var maxCorner = vec3.fromValues(Number.MIN_VALUE,Number.MIN_VALUE,Number.MIN_VALUE); // bbox corner
+            var minCorner = vec3.fromValues(Number.MAX_VALUE,Number.MAX_VALUE,Number.MAX_VALUE); // other corner
+            var temp = vec3.create(); // an intermediate vec3
+        
+            // process each triangle set to load webgl vertex and triangle buffers
+            numTriangleSets = inputTriangles.length; // remember how many tri sets
+            for (var whichSet=0; whichSet<numTriangleSets; whichSet++) { // for each tri set
+                
+                // set up hilighting, modeling translation and rotation
+                inputTriangles[whichSet].center = vec3.fromValues(0,0,0);  // center point of tri set
+                inputTriangles[whichSet].on = false; // not highlighted
+                inputTriangles[whichSet].translation = vec3.fromValues(0,0,0); // no translation
+                inputTriangles[whichSet].xAxis = vec3.fromValues(1,0,0); // model X axis
+                inputTriangles[whichSet].yAxis = vec3.fromValues(0,1,0); // model Y axis 
+
+                // set up the vertex and normal arrays, define model center and axes
+                inputTriangles[whichSet].glVertices = []; // flat coord list for webgl
+                inputTriangles[whichSet].glNormals = []; // flat normal list for webgl
+                inputTriangles[whichSet].glTextures = []; // flat texture list for webgl
+
+                var numVerts = inputTriangles[whichSet].vertices.length; // num vertices in tri set
+                for (whichSetVert=0; whichSetVert<numVerts; whichSetVert++) { // verts in set
+                    vtxToAdd = inputTriangles[whichSet].vertices[whichSetVert]; // get vertex to add
+                    normToAdd = inputTriangles[whichSet].normals[whichSetVert]; // get normal to add
+                    textureToAdd = inputTriangles[whichSet].uvs[whichSetVert]; // get texture to add
+
+                    inputTriangles[whichSet].glVertices.push(vtxToAdd[0],vtxToAdd[1],vtxToAdd[2]); // put coords in set coord list
+                    inputTriangles[whichSet].glNormals.push(normToAdd[0],normToAdd[1],normToAdd[2]); // put normal in set coord list
+                    inputTriangles[whichSet].glTextures.push(1-textureToAdd[0],textureToAdd[1]); // put texture coord list
+
+                    vec3.max(maxCorner,maxCorner,vtxToAdd); // update world bounding box corner maxima
+                    vec3.min(minCorner,minCorner,vtxToAdd); // update world bounding box corner minima
+                    vec3.add(inputTriangles[whichSet].center,inputTriangles[whichSet].center,vtxToAdd); // add to ctr sum
+                } // end for vertices in set
+                vec3.scale(inputTriangles[whichSet].center,inputTriangles[whichSet].center,1/numVerts); // avg ctr sum
+
+                // send the vertex coords and normals to webGL
+                vertexBuffers[whichSet] = gl.createBuffer(); // init empty webgl set vertex coord buffer
+                gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffers[whichSet]); // activate that buffer
+                gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(inputTriangles[whichSet].glVertices),gl.STATIC_DRAW); // data in
+                normalBuffers[whichSet] = gl.createBuffer(); // init empty webgl set normal component buffer
+                gl.bindBuffer(gl.ARRAY_BUFFER,normalBuffers[whichSet]); // activate that buffer
+                gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(inputTriangles[whichSet].glNormals),gl.STATIC_DRAW); // data in
+
+                textureBuffers[whichSet] = gl.createBuffer(); // init empty webgl set texture component buffer
+                gl.bindBuffer(gl.ARRAY_BUFFER,textureBuffers[whichSet]); // activate that buffer
+                gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(inputTriangles[whichSet].glTextures),gl.STATIC_DRAW); // data in
+
+                inputTriangles[whichSet].texture = loadTexture(inputTriangles[whichSet].material.texture);
+            
+                // set up the triangle index array, adjusting indices across sets
+                inputTriangles[whichSet].glTriangles = []; // flat index list for webgl
+                triSetSizes[whichSet] = inputTriangles[whichSet].triangles.length; // number of tris in this set
+                for (whichSetTri=0; whichSetTri<triSetSizes[whichSet]; whichSetTri++) {
+                    triToAdd = inputTriangles[whichSet].triangles[whichSetTri]; // get tri to add
+                    inputTriangles[whichSet].glTriangles.push(triToAdd[0],triToAdd[1],triToAdd[2]); // put indices in set list
+                } // end for triangles in set
+
+                // send the triangle indices to webGL
+                triangleBuffers.push(gl.createBuffer()); // init empty triangle index buffer
+                gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, triangleBuffers[whichSet]); // activate that buffer
+                gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,new Uint16Array(inputTriangles[whichSet].glTriangles),gl.STATIC_DRAW); // data in
+
+            } // end for each triangle set 
+            viewDelta = vec3.length(vec3.subtract(temp,maxCorner,minCorner)) / 100; // set global
+        } // end if triangle file loaded
+    } // end try 
+    
+    catch(e) {
+        console.log(e);
+    } // end catch
+} // end load models
+
+// setup the webGL shaders
+function setupShaders() {
+    
+    // define vertex shader in essl using es6 template strings
+    var vShaderCode = `
+        attribute vec3 aVertexPosition; // vertex position
+        attribute vec3 aVertexNormal; // vertex normal
+        attribute vec2 aTextureCoords;
+        
+        uniform mat4 umMatrix; // the model matrix
+        uniform mat4 upvmMatrix; // the project view model matrix
+        
+        varying vec3 vWorldPos; // interpolated world position of vertex
+        varying vec3 vVertexNormal; // interpolated normal for frag shader
+        varying vec2 vTextureCoords;
+
+        void main(void) {
+            
+            // vertex position
+            vec4 vWorldPos4 = umMatrix * vec4(aVertexPosition, 1.0);
+            vWorldPos = vec3(vWorldPos4.x,vWorldPos4.y,vWorldPos4.z);
+            gl_Position = upvmMatrix * vec4(aVertexPosition, 1.0);
+
+            // vertex normal (assume no non-uniform scale)
+            vec4 vWorldNormal4 = umMatrix * vec4(aVertexNormal, 0.0);
+            vVertexNormal = normalize(vec3(vWorldNormal4.x,vWorldNormal4.y,vWorldNormal4.z)); 
+
+            vTextureCoords = aTextureCoords;
+        }
+    `;
+    
+    // define fragment shader in essl using es6 template strings
+    var fShaderCode = `
+        precision mediump float; // set float to medium precision
+
+        uniform sampler2D uSampler;
+        uniform float uAlpha;
+
+        // eye location
+        uniform vec3 uEyePosition; // the eye's position in world
+        
+        // light properties
+        uniform vec3 uLightAmbient; // the light's ambient color
+        uniform vec3 uLightDiffuse; // the light's diffuse color
+        uniform vec3 uLightSpecular; // the light's specular color
+        uniform vec3 uLightPosition; // the light's position
+        
+        // material properties
+        uniform vec3 uAmbient; // the ambient reflectivity
+        uniform vec3 uDiffuse; // the diffuse reflectivity
+        uniform vec3 uSpecular; // the specular reflectivity
+        uniform float uShininess; // the specular exponent
+        
+        // geometry properties
+        varying vec3 vWorldPos; // world xyz of fragment
+        varying vec3 vVertexNormal; // normal of fragment
+        varying vec2 vTextureCoords;
+            
+        void main(void) {
+        
+            // ambient term
+            vec3 ambient = uAmbient*uLightAmbient; 
+            
+            // diffuse term
+            vec3 normal = normalize(vVertexNormal); 
+            vec3 light = normalize(uLightPosition - vWorldPos);
+            float lambert = max(0.0,dot(normal,light));
+            vec3 diffuse = uDiffuse*uLightDiffuse*lambert; // diffuse term
+            
+            // specular term
+            vec3 eye = normalize(uEyePosition - vWorldPos);
+            vec3 halfVec = normalize(light+eye);
+            float highlight = pow(max(0.0,dot(normal,halfVec)),uShininess);
+            vec3 specular = uSpecular*uLightSpecular*highlight; // specular term
+            
+            // combine to output color
+            vec4 texture = texture2D(uSampler, vTextureCoords);
+            vec3 colorOut = (ambient + diffuse + specular) * texture.rgb;
+            gl_FragColor = vec4(colorOut, texture.a*uAlpha);
+        }
+    `;
+    
+    try {
+        var fShader = gl.createShader(gl.FRAGMENT_SHADER); // create frag shader
+        gl.shaderSource(fShader,fShaderCode); // attach code to shader
+        gl.compileShader(fShader); // compile the code for gpu execution
+
+        var vShader = gl.createShader(gl.VERTEX_SHADER); // create vertex shader
+        gl.shaderSource(vShader,vShaderCode); // attach code to shader
+        gl.compileShader(vShader); // compile the code for gpu execution
+            
+        if (!gl.getShaderParameter(fShader, gl.COMPILE_STATUS)) { // bad frag shader compile
+            throw "error during fragment shader compile: " + gl.getShaderInfoLog(fShader);  
+            gl.deleteShader(fShader);
+        } else if (!gl.getShaderParameter(vShader, gl.COMPILE_STATUS)) { // bad vertex shader compile
+            throw "error during vertex shader compile: " + gl.getShaderInfoLog(vShader);  
+            gl.deleteShader(vShader);
+        } else { // no compile errors
+            var shaderProgram = gl.createProgram(); // create the single shader program
+            gl.attachShader(shaderProgram, fShader); // put frag shader in program
+            gl.attachShader(shaderProgram, vShader); // put vertex shader in program
+            gl.linkProgram(shaderProgram); // link program into gl context
+
+            if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) { // bad program link
+                throw "error during shader program linking: " + gl.getProgramInfoLog(shaderProgram);
+            } else { // no shader program link errors
+                gl.useProgram(shaderProgram); // activate shader program (frag and vert)
+                
+                // locate and enable vertex attributes
+                vPosAttribLoc = gl.getAttribLocation(shaderProgram, "aVertexPosition"); // ptr to vertex pos attrib
+                gl.enableVertexAttribArray(vPosAttribLoc); // connect attrib to array
+                vNormAttribLoc = gl.getAttribLocation(shaderProgram, "aVertexNormal"); // ptr to vertex normal attrib
+                gl.enableVertexAttribArray(vNormAttribLoc); // connect attrib to array
+
+                // Set up texture coordinate attribute and sampler
+                texCoordAttribLoc = gl.getAttribLocation(shaderProgram, "aTextureCoords");
+                gl.enableVertexAttribArray(texCoordAttribLoc);
+
+                samplerLoc = gl.getUniformLocation(shaderProgram, "uSampler");
+                alphaUniformLocation = gl.getUniformLocation(shaderProgram, "uAlpha");
+                
+                // locate vertex uniforms
+                mMatrixULoc = gl.getUniformLocation(shaderProgram, "umMatrix"); // ptr to mmat
+                pvmMatrixULoc = gl.getUniformLocation(shaderProgram, "upvmMatrix"); // ptr to pvmmat
+                
+                // locate fragment uniforms
+                var eyePositionULoc = gl.getUniformLocation(shaderProgram, "uEyePosition"); // ptr to eye position
+                var lightAmbientULoc = gl.getUniformLocation(shaderProgram, "uLightAmbient"); // ptr to light ambient
+                var lightDiffuseULoc = gl.getUniformLocation(shaderProgram, "uLightDiffuse"); // ptr to light diffuse
+                var lightSpecularULoc = gl.getUniformLocation(shaderProgram, "uLightSpecular"); // ptr to light specular
+                var lightPositionULoc = gl.getUniformLocation(shaderProgram, "uLightPosition"); // ptr to light position
+                ambientULoc = gl.getUniformLocation(shaderProgram, "uAmbient"); // ptr to ambient
+                diffuseULoc = gl.getUniformLocation(shaderProgram, "uDiffuse"); // ptr to diffuse
+                specularULoc = gl.getUniformLocation(shaderProgram, "uSpecular"); // ptr to specular
+                shininessULoc = gl.getUniformLocation(shaderProgram, "uShininess"); // ptr to shininess
+                
+                // pass global constants into fragment uniforms
+                gl.uniform3fv(eyePositionULoc,Eye); // pass in the eye's position
+                gl.uniform3fv(lightAmbientULoc,lightAmbient); // pass in the light's ambient emission
+                gl.uniform3fv(lightDiffuseULoc,lightDiffuse); // pass in the light's diffuse emission
+                gl.uniform3fv(lightSpecularULoc,lightSpecular); // pass in the light's specular emission
+                gl.uniform3fv(lightPositionULoc,lightPosition); // pass in the light's position
+            } // end if no shader program link errors
+        } // end if no compile errors
+    } // end try 
+    
+    catch(e) {
+        console.log(e);
+    } // end catch
+} // end setup shaders
+
+function main() {
+    setupWebGL();
+    loadModels();
+    setupShaders();
+
+}
